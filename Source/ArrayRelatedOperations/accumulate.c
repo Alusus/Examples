1066d61fb996891a978c5704e1448da81ffd382d
KaisHasan <kais.hasan314@gmail.com>
2023-09-27 22:44:57 +0300

    add missing parameter

--- Source/ArrayRelatedOperations/accumulate.c
+++ Source/ArrayRelatedOperations/accumulate.c
@@ -1,4 +1,4 @@
-void accumulate(int* a, int* result, int size) {
+void accumulate(int* a, int* result, int size, int (*f)(int, int)) {
     result[0] = a[0];
     for(int i = 1; i < size; i++) {
         result[i] = f(result[i-1], a[i]);
