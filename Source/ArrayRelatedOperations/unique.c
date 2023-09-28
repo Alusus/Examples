8b7d8d419116486cd019018dbdfc8046e04a2120
KaisHasan <kais.hasan314@gmail.com>
2023-09-28 10:35:20 +0300

    change the way we find unique elements in C

--- Source/ArrayRelatedOperations/unique.c
+++ Source/ArrayRelatedOperations/unique.c
@@ -1,12 +1,33 @@
-#define MAX 100000
+void findUniqueElements(int arr[], int n) {
+    // الجرد لحفظ العناصر الفريدة
+    int uniqueElements[n];
+    int uniqueCount = 0;
 
-void unique(int a[], int n, int result[], int *k) {
-    int hash[MAX] = {0};
+    // الجرد للتحقق من تكرار العناصر
+    int isUnique;
 
+    // تمرير عبر المصفوفة
     for (int i = 0; i < n; i++) {
-        if (hash[a[i]] == 0) {
-            result[(*k)++] = a[i];
-            hash[a[i]] = 1;
+        isUnique = 1;
+
+        // التحقق من تكرار العنصر في الجزء السابق من المصفوفة
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+
+        // إذا كان العنصر فريدًا ، قم بإضافته إلى الجرد
+        if (isUnique) {
+            uniqueElements[uniqueCount++] = arr[i];
         }
     }
+
+    // طباعة العناصر الفريدة
+    printf("العناصر الفريدة في المصفوفة:\n");
+    for (int i = 0; i < uniqueCount; i++) {
+        printf("%d ", uniqueElements[i]);
+    }
+    printf("\n");
 }
