bd45290dc4d510e48ffa6d2e738337deffe7edf4
KaisHasan <kais.hasan314@gmail.com>
2023-10-24 08:39:29 +0300

    use 0-indexing

--- Source/ArrayRelatedOperations/insert.c
+++ Source/ArrayRelatedOperations/insert.c
@@ -1,8 +1,8 @@
 void insert(int a[], int n, int pos, int x) {
     // Shift elements to right
-    for (int i = n; i >= pos; i--)
+    for (int i = n; i > pos; i--)
         a[i] = a[i - 1];
 
     // Insert x at pos
-    a[pos - 1] = x;
+    a[pos] = x;
 }
