96e21f3a95f8b699261dae402c175863d1bc094d
KaisHasan <kais.hasan314@gmail.com>
2024-07-07 09:45:37 +0300

    update the training script to use rag dataset

--- train_script.py
+++ train_script.py
@@ -6,22 +6,22 @@ version = input("Enter the model version: ")
 client = OpenAI(api_key=api_key)
 
 train_file = client.files.create(
-  file=open("train_set.jsonl", "rb"),
+  file=open("rag_train_set.jsonl", "rb"),
   purpose="fine-tune"
 )
 
 test_file = client.files.create(
-  file=open("test_set.jsonl", "rb"),
+  file=open("rag_test_set.jsonl", "rb"),
   purpose="fine-tune"
 )
 
 client.fine_tuning.jobs.create(
   training_file=train_file.id,
   validation_file=test_file.id,
-  suffix=f"_v{version}_Alusus",
+  suffix=f"rag_v{version}_Alusus",
   model="gpt-3.5-turbo",
-  hyperparameters={
-    "n_epochs":3
-  }
+  # hyperparameters={
+  #   "n_epochs":3
+  # }
 )
 
